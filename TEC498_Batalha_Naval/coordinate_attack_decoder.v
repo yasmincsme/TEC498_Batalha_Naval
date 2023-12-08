dcc15cbc59f4e7bd3df5020c7f2421a4e073ee38
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2023-12-08 17:06:26 -0300

    Delete TEC498_Batalha_Naval directory

--- TEC498_Batalha_Naval/coordinate_attack_decoder.v
+++
@@ -1,9 +0,0 @@
-module coordinate_attack_decoder(input [2:0]A, output [3:0]X);//Decodificador para coordenadas de ataque - colunas
-	
-	and And0(X[3], 1'b1, 1'b1);
-	and And1(X[2], A[2], 1'b1);
-	
-	and And2(X[1], !A[2], A[1]);
-	and And3(X[0], !A[2], A[0]);
-	
-endmodule 
--- TEC498_Batalha_Naval/counter_3bit.v
+++
@@ -1,8 +0,0 @@
-module counter_3bit(input clk, output [2:0]X);
-
-	flipflop_T fft0(.clk(clk), .t(1'b1), .q(X[0]));
-	flipflop_T fft1(.clk(X[0]), .t(1'b1), .q(X[1]));
-	flipflop_T fft2(.clk(X[1]), .t(1'b1), .q(X[2]));
-
-
-endmodule 
